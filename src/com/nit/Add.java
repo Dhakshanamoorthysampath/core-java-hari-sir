6f49d28f7055c1a6e2f235b09666b894da523998
Dhakshanamoorthysampath <dhakshanamoorthysampath@gmail.com>
2026-09-24 02:51:25 -0700

    core commit

---
+++ src/com/nit/Add.java
@@ -0,0 +1,17 @@
+package com.nit;
+
+public class Add {
+
+	
+	public int sum(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Add obj = new Add();
+		System.out.println(obj.sum(10,20));
+
+	}
+
+}
