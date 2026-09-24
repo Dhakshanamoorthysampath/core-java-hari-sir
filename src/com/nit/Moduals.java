28d9cefe1b9dc7267406ca8839ba9b66dcb709ab
Dhakshanamoorthysampath <dhakshanamoorthysampath@gmail.com>
2026-09-24 04:02:27 -0700

    modules add sucess fully

---
+++ src/com/nit/Moduals.java
@@ -0,0 +1,14 @@
+package com.nit;
+
+public class Moduals {
+
+public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+	int c = a%b;//
+	int e =a/b;
+	System.out.println(c);
+	System.out.println(e);
+	
+}
+}
