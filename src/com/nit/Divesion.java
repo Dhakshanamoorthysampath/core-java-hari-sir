5a76936edd1d5239340d75ade9285439b86a9f9b
Dhakshanamoorthysampath <dhakshanamoorthysampath@gmail.com>
2026-09-24 03:42:49 -0700

    second changes

--- src/com/nit/Divesion.java
+++ src/com/nit/Divesion.java
@@ -6,6 +6,20 @@ public class Divesion {
 		int a=10;
 		int b=20;
 		int c=a/b;
-		System.out.println(c);
+	//	System.out.println(c);
+		
+		String day = IO.readln("Enter the day");	
+	String weakday = weekday(day);
+	System.out.println(weakday);
+	}
+	static String weekday(String day) {
+	
+		if(day=="monday") { 
+			return "monday";
+		}else if(day=="tuesday") {
+			return "tuesday";
+		}
+		
+		return "method executed";
 	}
 }
